7d13b012b7bc297530f187f128b20f21e86a5afe
fboehm <frederick.boehm@gmail.com>
2016-01-29 14:22:58 -0600

    updated calc_rho()

--- R/update_K.R
+++ R/update_K.R
@@ -20,7 +20,8 @@ calc_allocation_prob <- function(y, w, # w a scalar
 #' @param omega_small list containing the current state of parameter vector
 #' @param omega_big list containing the proposed state of parameter vector
 #' @param y data vector
-#' @param ind indicator denoting which cluster is affected
+#' @param ind1 indicator denoting which cluster is affected
+#' @param ind2 indicator denoting other affected cluster
 #' @param a hyperparameter scalar
 #' @param b a scalar hyperparameter
 #' @param alpha scalar parameter for dimension-matching
@@ -28,24 +29,24 @@ calc_allocation_prob <- function(y, w, # w a scalar
 #' @param r scalar parameter for dimension-matching
 #' @param delta scalar hyperparameter
 #' @export
-calc_rho <- function(y, omega_small, omega_big, ind, a, b, alpha, beta, r, delta){
+calc_rho <- function(y, omega_small, omega_big, ind1, ind2, a, b, alpha, beta, r, delta){
   # work in one-dimension
   ##########
   # unpack omega
   K_big <- omega_big$K
-  kappa_big <- omega_big$kappa[c(ind, K_big)]
+  kappa_big <- omega_big$kappa[c(ind1, ind2)]
   s_big <- omega_big$s # use full vector, s
-  w_big <- omega_big$w[c(ind, K_big)]
+  w_big <- omega_big$w[c(ind1, ind2)]
   #unpack omega_prop
-  kappa_small <- omega_small$kappa[ind]
+  kappa_small <- omega_small$kappa[ind1]
   s_small <- omega_small$s
-  w_small <- omega_small$w[ind]
+  w_small <- omega_small$w[ind1]
   K_small <- omega_small$K
   # calc kappa ratio
   kappa_ratio <- (1 / gamma(a / 2)) * (kappa_big[1]) ^ (1 - a / 2) * kappa_big[2] * (b / (2 * kappa_big[2])) ^ (a / 2) * exp(- 0.5 * b * (1 / kappa_big[1] + 1 / kappa_big[2] - 1 / kappa_small)) / kappa[ind] ^ (1 - a / 2)
   # calc w ratio
   n_big <- numeric(length = 2)
-  n_big[1] <- sum(s_big == ind)
+  n_big[1] <- sum(s_big == ind1)
   n_big[2] <- sum(s_big == K_big)
   w_ratio <- w_big[1] ^ (delta - 1 + n_big[1]) * w_big[2] ^ (delta - 1 + n_big[2]) / (w_small ^ (delta - 1 + n_new[1] + n_new[2]) * beta(delta, K_small * delta))
   # calc mu ratio
@@ -91,33 +92,36 @@ update_K <- function(y, mu, w, kappa, s, tau, theta, delta){
   alpha <- rbeta(n = 1, 1, 1)
   beta <- rbeta(n = 1, 1, 1)
   r <- rbeta(n = 1, 2, 2)
+  # define s_new
+  s_new <- s
   ### decide to split or combine
   split <- as.logical(rbinom(n = 1, size = 1, prob = 1 - q_down))
+  ##########
   if (split){
     omega_small <- list(K = K, mu = mu, kappa = kappa, w = w, s = s)
-    ind <- sample(1:K, size = 1, replace = FALSE)
+    ind1 <- sample(1:K, size = 1, replace = FALSE)
+    ind2 <- K + 1
     ## edit w; make K+1 the 'new' component
     w_new <- w
-    w_new[ind] <- alpha * w[ind]
-    w_new[K + 1] <- (1 - alpha) * w[ind]
+    w_new[ind1] <- alpha * w[ind1]
+    w_new[ind2] <- (1 - alpha) * w[ind1]
     ## edit mu
     mu_new <- mu
-    mu_new[ind] <- mu[ind] - sqrt(w_new[K + 1] / w_new[ind]) * r / sigma[ind]
-    mu_new[K + 1] <- mu[ind] + sqrt(w_new[ind] / w_new[K + 1]) * r / sigma[ind]
+    mu_new[ind1] <- mu[ind1] - sqrt(w_new[ind2] / w_new[ind1]) * r / sigma[ind1]
+    mu_new[ind2] <- mu[ind1] + sqrt(w_new[ind1] / w_new[ind2]) * r / sigma[ind1]
     ## edit kappa (sigma)
     kappa <- 1 / sigma ^ 2
     kappa_new <- kappa
-    kappa_new[ind] <- beta * (1 - r) ^ 2 * (w[ind] / w_new[ind]) * kappa[ind]
-    kappa_new[K + 1] <- (1 - beta) * (1 - r) ^ 2 * (w[ind] / w_new[K + 1]) * kappa[ind]
+    kappa_new[ind1] <- beta * (1 - r) ^ 2 * (w[ind1] / w_new[ind1]) * kappa[ind1]
+    kappa_new[ind2] <- (1 - beta) * (1 - r) ^ 2 * (w[ind1] / w_new[ind2]) * kappa[ind1]
     ## Yanxun told me to see Richardson & Green 1997
     # to get the method for
     # allocation, ie, to assign values to n_new
-    s_new <- s
-    foo <- rbinom(n = sum(s == ind), size = 1, prob = calc_allocation_prob(y[s == ind], w_new[ind], mu_new[ind], kappa_new[ind]) / (calc_allocation_prob(y[s == ind], w_new[ind], mu_new[ind], kappa_new[ind]) + calc_allocation_prob(y[s == ind], w_new[K + 1], mu_new[K + 1], kappa_new[K + 1])))
-    s_new[s == ind] <- ind * foo + (K + 1) * (1 - foo)
+    foo <- rbinom(n = sum(s == ind1), size = 1, prob = calc_allocation_prob(y[s == ind1], w_new[ind1], mu_new[ind1], kappa_new[ind1]) / (calc_allocation_prob(y[s == ind1], w_new[ind1], mu_new[ind1], kappa_new[ind1]) + calc_allocation_prob(y[s == ind1], w_new[ind2], mu_new[ind2], kappa_new[ind2])))
+    s_new[s == ind1] <- ind1 * foo + (ind2) * (1 - foo)
     ############
     omega_big <- list(K = K + 1, mu = mu_new, kappa = kappa_new, w = w_new, s = s_new)
-    acc_ratio <- calc_rho(y, omega_small, omega_big, ind, a, b, alpha, beta, r)
+    acc_ratio <- calc_rho(y, omega_small, omega_big, ind1, ind2, a, b, alpha, beta, r)
     u <- runif(n = 1, min = 0, max = 1)
     # compare u to acceptance ratio & decide to accept or reject
     if (u < acc_ratio) {out <- list(w = w_new, mu = mu_new, kappa = kappa_new, s = s_new)} else {out <- list(w = w, mu = mu, kappa = kappa, s = s)}
@@ -140,6 +144,8 @@ update_K <- function(y, mu, w, kappa, s, tau, theta, delta){
     kappa_new <- kappa
     kappa_new[ind1] <- (w[ind1]/w_new[ind1])*kappa[ind1] + (w[ind2]/w_new[ind1])*kappa[ind2] + (w[ind1]*w[ind2]/(w_new[ind1])^2)*(mu[ind1] - mu[ind2])^2
     kappa_new <- kappa_new[-ind2]
+    # edit s
+    s_new[ind1] <- s[ind1] + s[ind2]
     # calculate acceptance ratio
     omega_small <- list(K = K - 1, mu = mu_new, kappa = kappa_new, w = w_new, s = s_new)
     omega_big <- list(K = K, mu = mu, kappa = kappa, w = w, s = s)
